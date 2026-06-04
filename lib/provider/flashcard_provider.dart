a1136be05a9f8816d4b067a385a43bd06de41b24
Ananaya <ananaya20may@gmail.com>
2026-06-04 23:45:20 +0530

    flashcard_provider

---
+++ lib/provider/flashcard_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+import '../models/flashcard.dart';
+
+class FlashcardProvider extends ChangeNotifier {
+
+  // The master list of all flashcards
+  List<Flashcard> _cards = [
+    // Some starter cards so your app isn't empty on first run
+    Flashcard(id: '1', question: 'What is Flutter?', answer: 'A UI toolkit by Google for building cross-platform apps'),
+    Flashcard(id: '2', question: 'What language does Flutter use?', answer: 'Dart'),
+    Flashcard(id: '3', question: 'What is a Widget?', answer: 'The basic building block of Flutter UI'),
+  ];
+
